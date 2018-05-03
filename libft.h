200e1327c4fc91d48e4bf1121e27f46e0d7a6dc6
Nataly Costa <ncosta@e1z4r6p21.42.us.org>
2018-05-03 14:35:32 -0700

    aa

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ncosta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 10:54:15 by ncosta            #+#    #+#             */
-/*   Updated: 2018/05/03 13:37:40 by ncosta           ###   ########.fr       */
+/*   Updated: 2018/05/03 14:35:24 by ncosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,6 +76,9 @@ t_list *ft_lstnew(void const *content, size_t content_size);
 void	ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstadd(t_list **alst, t_list *new);
+void ft_putendl_fd(char const *s, int fd);
+void ft_putnbr_fd(int n, int fd);
+void ft_putnbr(int n);
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list *ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
