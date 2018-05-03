cc95e0b2035bc43ade57bb0a7eb5d2cd23910d0a
Nataly Costa <ncosta@e1z4r6p21.42.us.org>
2018-05-03 14:19:09 -0700

    aa

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: ncosta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/26 18:18:51 by ncosta            #+#    #+#             */
-/*   Updated: 2018/05/03 14:12:53 by ncosta           ###   ########.fr       */
+/*   Updated: 2018/05/03 14:18:55 by ncosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 void	*ft_memalloc(size_t size)
 {
-	void *mem;
+	char	*r;
+	size_t	i;
 
-	mem = (void *)malloc(sizeof(void) *  size);
-	if (!mem || size == 0)
-	{
+	if (!(r = (char *)malloc(size)))
 		return (NULL);
-	}
-	ft_bzero(mem, size);
-	return (mem);
+	i = 0;
+	while (i < size)
+		r[i++] = '\0';
+	return (r);
+
 }
