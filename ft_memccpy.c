482472d3947053f2d0f1c30111db6c98028490fa
Nataly Costa <ncosta@e1z3r5p20.42.us.org>
2018-05-10 14:33:10 -0700

    ft_memmove modified

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: ncosta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 10:44:23 by ncosta            #+#    #+#             */
-/*   Updated: 2018/05/02 19:05:54 by ncosta           ###   ########.fr       */
+/*   Updated: 2018/05/10 14:08:27 by ncosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	size_t	i;
-	char	*dst2;
-	char	*src2;
+	char	*dst1;
+	char	*src1;
 
 	i = 0;
-	dst2 = (char *)dst;
-	src2 = (char *)src;
+	dst1 = (char *)dst;
+	src1 = (char *)src;
 	while (i < n)
 	{
-		dst2[i] = src2[i];
-		if ((unsigned char)src2[i] == (unsigned char)c)
+		dst1[i] = src1[i];
+		if ((unsigned char)src1[i] == (unsigned char)c)
+		{
 			return ((char *)dst + i + 1);
+		}
 		i++;
 	}
 	return (NULL);
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,32 +6,19 @@
 /*   By: ncosta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/03 19:54:53 by ncosta            #+#    #+#             */
-/*   Updated: 2018/05/03 19:55:17 by ncosta           ###   ########.fr       */
+/*   Updated: 2018/05/10 14:29:18 by ncosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dest, const void *src, size_t len)
 {
-	size_t			i;
-	unsigned char	*s;
-	unsigned char	*d;
+	void	*s1;
 
-	d = (unsigned char*)dst;
-	s = (unsigned char*)src;
-	i = 0;
-	if (src > dst)
-		while (i < len)
-		{
-			d[i] = s[i];
-			i++;
-		}
-	else
-		while (len > 0)
-		{
-			len--;
-			d[len] = s[len];
-		}
-	return (dst);
+	s1 = ft_memalloc(n + 1);
+	s1 = ft_memcpy(s1, src, n);
+	dest = ft_memcpy(dest, s1, n);
+	free(s1);
+	return (dest);
 }
