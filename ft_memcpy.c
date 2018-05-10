dbb5a56d22252857516286db0fc65cd091084b7b
Nataly Costa <ncosta@e1z3r5p20.42.us.org>
2018-05-10 13:52:19 -0700

    ft_memset modified

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ncosta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 10:43:52 by ncosta            #+#    #+#             */
-/*   Updated: 2018/05/02 19:05:05 by ncosta           ###   ########.fr       */
+/*   Updated: 2018/05/10 13:50:51 by ncosta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	char	*d;
-	char	*s;
+	char	*dst1;
+	char	*src1;
 
 	i = 0;
-	d = (char *)dst;
-	s = (char *)src;
+	dst1 = (char *)dst;
+	src1 = (char *)src;
 	while (i < n)
 	{
-		d[i] = s[i];
+		dst1[i] = src1[i];
 		i++;
 	}
 	return (dst);
